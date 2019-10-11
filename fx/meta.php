0e2892a540a3de6c2063414a4fa1f46a02e45d22
Mark Parnell <mark@sparkweb.com.au>
2019-10-11 11:04:10 +1100

    Added helper functions to get single-ised user and term meta

--- fx/meta.php
+++ fx/meta.php
@@ -34,6 +34,88 @@ function spark_get_post_meta($post = null, $key = '') {
 }
 
 /**
+ * Gets meta for a user
+ * @param integer|WP_User $user Optional. User to retrieve meta for
+ * @param string $key Optional. Meta key to retrieve value for
+ * @return array|mixed If key is empty will return array of meta values already single-ised, else will return value for the specified key
+ */
+function spark_get_user_meta($user = null, $key = '') {
+    if (!$user instanceof WP_User) {
+        if (is_null($user)) {
+            $user = get_current_user_id();
+        }
+        $user = new WP_User($user);
+    }
+    $meta = false;
+    if ($user instanceof WP_User) {
+        $transients = Spark_Transients::use_transients(); // Set this to false to force all transients to refresh
+        $transient = ns_.'usermeta_'.$user->ID.'_';
+        if (false === $transients) {
+            delete_transient($transient);
+        }
+        if (false === ($meta = get_transient($transient))) {
+            $meta = get_user_meta($user->ID);
+            if (is_array($meta)) {
+                $meta = spark_rationalise_meta($meta);
+            }
+            if (true === $transients) {
+                set_transient($transient, $meta, LONG_TERM);
+            }
+        }
+        unset($transient);
+
+        if (!empty($key)) {
+            return $meta[$key];
+        }
+    }
+
+    return $meta;
+}
+
+/**
+ * Gets meta for a term
+ * @param integer|WP_Term $term Optional. Term to retrieve meta for.
+ * @param string $key Optional. Meta key to retrieve value for.
+ * @return array|mixed If key is empty will return array of meta values already single-ised, else will return value for the specified key
+ */
+function spark_get_term_meta($term = null, $taxonomy = null, $key = '') {
+    if (!$term instanceof WP_Term) {
+        if (is_null($term)) {
+            $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+        } else {
+            if (is_null($taxonomy)) {
+                $taxonomy = get_query_var('taxonomy');
+            }
+            $term = get_term_by('id', $term, $taxonomy);
+        }
+    }
+    $meta = false;
+    if ($term instanceof WP_Term) {
+        $transients = Spark_Transients::use_transients(); // Set this to false to force all transients to refresh
+        $transient = ns_.'termmeta_'.$term->term_id.'_';
+        if (false === $transients) {
+            delete_transient($transient);
+        }
+        if (false === ($meta = get_transient($transient))) {
+            $meta = get_term_meta($term->term_id);
+            if (is_array($meta)) {
+                $meta = spark_rationalise_meta($meta);
+            }
+            if (true === $transients) {
+                set_transient($transient, $meta, LONG_TERM);
+            }
+        }
+        unset($transient);
+
+        if (!empty($key)) {
+            return $meta[$key];
+        }
+    }
+
+    return $meta;
+}
+
+/**
  * Clean up meta array to include single values as direct value, like $single parameter to get_post_meta()
  * @param array $meta
  * @return array
