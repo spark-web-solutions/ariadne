5ebf2c23e90d8dd26dc43034b07e67e39a3a6f39
Mark Parnell <mark@sparkweb.com.au>
2020-02-21 15:26:10 +1100

    Removed Browser Update script

--- footer.php
+++ footer.php
@@ -14,19 +14,6 @@ Spark_Theme::section('name=copyright&file=copyright.php&class=hide-for-print ful
             if (typeof ajaxurl === 'undefined') {
                 var ajaxurl = '<?php echo admin_url('admin-ajax.php'); ?>';
             }
-<?php if (!is_admin()) { ?>
-            var $buoop = {c:2};
-            function $buo_f() {
-                var e = document.createElement("script");
-                e.src = "//browser-update.org/update.js";
-                document.body.appendChild(e);
-            }
-<?php } ?>
-            try {
-                document.addEventListener("DOMContentLoaded", $buo_f, false);
-            } catch(e) {
-                window.attachEvent("onload", $buo_f);
-            }
         </script>
     </body>
 </html>
