013e7ef600614910b62a09ed39780a8d0efe3d6e
Mark Parnell <mark@sparkweb.com.au>
2023-05-05 11:29:47 +1000

    Added search form above search results

--- templates/sections/archive.php
+++ templates/sections/archive.php
@@ -16,8 +16,15 @@ echo '<!-- START: '.$file.' -->'."\n";
 		<div class="small-24 cell no-sidebar">
 <?php
 if ($archive_page) {
-	echo '	<h1>'.$archive_page->post_title.'</h1>'."\n";
+	if (!empty(spark_get_post_meta($archive_page, 'hide_title'))) {
+?>
+			<h1><?php echo $archive_page->post_title; ?></h1>
+<?php
+	}
 	echo apply_filters('the_content', $archive_page->post_content);
+}
+if (is_search()) {
+	get_search_form();
 }
 ?>
 			<div class="grid-x grid-margin-x small-up-1 medium-up-2 large-up-3" data-equalizer data-equalize-by-row="true">
@@ -26,7 +33,9 @@ while (have_posts()) {
 	the_post();
 	echo spark_get_card('card=post-preview&ID='.get_the_id());
 }
-echo '</div>'."\n";
+?>
+			</div>
+<?php
 echo spark_foundation_pagination();
 ?>
 		</div>
